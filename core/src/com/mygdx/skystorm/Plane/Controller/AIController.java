6c54be969c38e4a788a2b4dcfc526781843534e4
Plastix <aidanpieper@gmail.com>
2015-01-30 23:47:26 -0500

    Stub out controllers

---
+++ core/src/com/mygdx/skystorm/Plane/Controller/AIController.java
@@ -0,0 +1,5 @@
+package com.mygdx.skystorm.Plane.Controller;
+
+public abstract class AIController extends Controller {
+
+}
---
+++ core/src/com/mygdx/skystorm/Plane/Controller/PlayerController.java
@@ -0,0 +1,11 @@
+package com.mygdx.skystorm.Plane.Controller;
+
+import com.mygdx.skystorm.Plane.PlaneBody;
+
+public class PlayerController extends Controller {
+
+    @Override
+    public void control(PlaneBody planeBody) {
+
+    }
+}
