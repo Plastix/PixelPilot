c7621829acf2a036f6be9c9cecbe0592b5125acf
JEphron <jephron63@gmail.com>
2015-02-08 14:39:27 -0500

    Fixed mode select layout and button backgrounds

--- core/src/com/mygdx/skystorm/screen/ModeSelectScreen.java
+++ core/src/com/mygdx/skystorm/screen/ModeSelectScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.skystorm.screen;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Scaling;
 import com.mygdx.skystorm.SkyStorm;
 import com.mygdx.skystorm.data.Resources;
 import com.mygdx.skystorm.screen.ui.Button;
@@ -21,25 +23,33 @@ public class ModeSelectScreen extends ActionScreen {
     }
 
     public void createMenuButtons(){
+
+        Table root = new Table();
+        root.setFillParent(true);
+
         arcadeModeButton = new Button("Arcade",
-                new Texture(Resources.btn_play_up_img),
-                new Texture(Resources.btn_play_down_img),
+                new Texture(Resources.menu_button),
+                new Texture(Resources.menu_button_down),
                 new Runnable() {
             @Override
             public void run() {
 //                game.setScreen(new ArcadeGameScreen(game));
             }
         });
+        arcadeModeButton.setScaling(Scaling.fit);
+        root.add(arcadeModeButton).pad(20);
 
         campaignModeButton = new Button("Campaign",
-                new Texture(Resources.btn_play_up_img),
-                new Texture(Resources.btn_play_down_img),
+                new Texture(Resources.menu_button),
+                new Texture(Resources.menu_button_down),
                 new Runnable() {
                     @Override
                     public void run() {
 //                game.setScreen(new ArcadeGameScreen(game));
                     }
                 });
-
+        root.add(campaignModeButton);
+        campaignModeButton.setScaling(Scaling.fit);
+        stage.addActor(root);
     }
 }
