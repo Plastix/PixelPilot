9020894ab77917ac59f3a509046b9efe7d07f347
Plastix <aidanpieper@gmail.com>
2015-02-11 20:05:35 -0500

    Rename turnSpeed to turnRadius

--- core/src/com/mygdx/skystorm/Plane/PlaneDefinition.java
+++ core/src/com/mygdx/skystorm/Plane/PlaneDefinition.java
@@ -6,13 +6,13 @@ public class PlaneDefinition {
     public String name;
     public int speed;
     public int armor;
-    public int turnSpeed;
+    public int turnRadius;
     public String spritePath;
 
     @Override
     public String toString(){
-        return String.format("name: %s, speed: %s, armor: %s, turnSpeed: %s, spritePath: %s",
-                name, speed, armor, turnSpeed, spritePath);
+        return String.format("name: %s, speed: %s, armor: %s, turnRadius: %s, spritePath: %s",
+                name, speed, armor, turnRadius, spritePath);
     }
 
 }
