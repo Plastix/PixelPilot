7a1d60e69811cd834679142ca5146ed80d2da373
JEphron <jephron63@gmail.com>
2015-02-16 21:59:02 -0500

    Created tracking camera

--- core/src/com/mygdx/skystorm/util/Utils.java
+++ core/src/com/mygdx/skystorm/util/Utils.java
@@ -1,6 +1,7 @@
 package com.mygdx.skystorm.util;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Utils {
@@ -20,6 +21,17 @@ public class Utils {
 
     public static Vector2 positionVector(Actor target) {
         return new Vector2(target.getX(), target.getY());
+    }
+
+    public static Vector2 vec3d2d(Vector3 vec3) {
+        return new Vector2(vec3.x, vec3.y);
+    }
+    public static Vector3 vec2d3d(Vector2 vec2) {
+        return new Vector3(vec2.x, vec2.y, 0);
+    }
+
+    public static float map(float val, float inMin, float inMax, float outMin, float outMax){
+        return ((val - inMin)/(inMax - inMin)) * (outMax - outMin) + outMin;
     }
 
 }
