b78d4dd09227b32e214bbea5bb29bc3de724dbd2
Plastix <aidanpieper@gmail.com>
2015-03-01 18:16:32 -0500

    Basic Accelerometer input

--- core/src/com/mygdx/pixelpilot/plane/controller/PlayerController.java
+++ core/src/com/mygdx/pixelpilot/plane/controller/PlayerController.java
@@ -3,13 +3,26 @@ package com.mygdx.pixelpilot.plane.controller;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.mygdx.pixelpilot.plane.Plane;
+import com.mygdx.pixelpilot.util.Utils;
 
 public class PlayerController extends Controller {
 
     float turnAmount = 0.5f;
+    boolean accelerometer;
+
+    public PlayerController() {
+        accelerometer = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
+    }
 
     @Override
     public void control(Plane planeBody) {
+
+        if(accelerometer){
+            float turn = Utils.map(Gdx.input.getAccelerometerY(), -5, 5, -1, 1);
+            planeBody.turn(-turn);
+        }
+
+        //Desktop Controls
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
             turnAmount = Math.min(turnAmount + 0.05f, 1f);
         }
